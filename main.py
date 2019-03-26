27cb5ad3d2717a2e0275796cace41d381a9740d3
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-03-26 20:13:29 +0100

    wip edit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 import webbrowser
+import datetime
 
 from jj import startup_behaviour, Action, HELP_TEXT
 from search import search_for
@@ -11,6 +12,7 @@ from mdformat import render_html
 
 
 HTML_TMP_FILE = '/tmp/search_result.html'
+EDIT_TMP_FILE = '/tmp/jj.txt'
 
 
 def load_journal(journal_path):
@@ -18,12 +20,32 @@ def load_journal(journal_path):
         return json.loads(f.read())
 
 
+def save_journal(journal, journal_path):
+    with open(journal_path, 'w') as f:
+        return f.write(json.dumps(journal, indent=2))
+
+
+def today():
+    now = datetime.datetime.now()
+    return datetime.date(now.year, now.month, now.day)
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     journal_path = os.environ.get('JOURNAL', None)
     result = startup_behaviour(args, journal_path)
     if result == Action.EDIT:
-        print("EDIT")
+        journal = load_journal(journal_path)
+        date = today().strftime("%Y-%m-%d")
+        if date not in journal:
+            journal[date] = "short desc - longer desc\n   some\n   code"
+        with open(EDIT_TMP_FILE, 'w') as f:
+            f.write(journal[date])
+        os.system('gedit ' + EDIT_TMP_FILE)
+        with open(EDIT_TMP_FILE) as f:
+            entry = f.read()
+        journal[date] = entry
+        save_journal(journal, journal_path)
     elif result == Action.SEARCH:
         keyword = sys.argv[1]
         journal = load_journal(journal_path)
